41cc5fc2ac7013cedae7c68578b2d39ed6ce51eb
koturn <jeak.koutan.apple@gmail.com>
2016-10-16 01:36:42 +0900

    Implement denite.nvim source

---
+++ rplugin/python3/denite/kind/mplayer.py
@@ -0,0 +1,19 @@
+"""
+FILE: mplayer.vim
+AUTHOR: koturn <jeak.koutan.apple@gmail.com>
+DESCRIPTION: {{{
+Mplayer frontend of Vim.
+denite.nvim: https://github.com/Shougo/
+}}}
+"""
+
+from .base import Base
+
+
+class Kind(Base):
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.name = 'mplayer'
+
+    def action_default(self, context):
+        self.vim.call('vim#mplayer#cmd#enqueue', list(map(lambda e: e['action__path'], context['targets'])))
---
+++ rplugin/python3/denite/source/mplayer.py
@@ -0,0 +1,35 @@
+"""
+FILE: mplayer.vim
+AUTHOR: koturn <jeak.koutan.apple@gmail.com>
+DESCRIPTION: {{{
+Mplayer frontend of Vim.
+denite.nvim: https://github.com/Shougo/
+}}}
+"""
+
+from .base import Base
+import itertools
+import os
+import glob
+
+
+class Source(Base):
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.name = 'mplayer'
+        self.kind = 'mplayer'
+
+    def gather_candidates(self, context):
+        path = os.path.abspath(os.path.expanduser((context['args'][0] if len(context['args']) > 0 else self.vim.eval('g:mplayer#default_dir'))))
+        if path[-1] != '/':
+            path += '/'
+        path_length = len(path)
+        suffixes = self.vim.eval("get(g:, 'mplayer#suffixes', ['*'])")
+        glob_patterns = ['*'] if len(suffixes) == 0 else suffixes
+        filelist = itertools.chain.from_iterable(
+                map(lambda glob_pattern: glob.glob(path + '*.' + glob_pattern), glob_patterns))
+        return list(map(
+            lambda filepath: {
+                'word': filepath[path_length: ],
+                'action__path': filepath},
+            filelist))
